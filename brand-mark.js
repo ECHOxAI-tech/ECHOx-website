21664d42dd8722039fad0bc38de35669f17c15d9
Admin <admin@Admins-MacBook-Air.fritz.box>
2026-07-14 18:21:26 +0200

    Restore raised ECHOx brand mark

---
+++ brand-mark.js
@@ -0,0 +1,29 @@
+(() => {
+  const style = document.createElement('style');
+  style.textContent = '.echox-mark-x{display:inline-block;font-size:.62em;line-height:1;position:relative;top:-.34em;margin:0 .015em}';
+  document.head.appendChild(style);
+
+  const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
+  const nodes = [];
+  while (walker.nextNode()) {
+    const node = walker.currentNode;
+    if (/ECHOx/.test(node.nodeValue) && !node.parentElement.closest('script, style, textarea, svg')) nodes.push(node);
+  }
+
+  nodes.forEach(node => {
+    const parts = node.nodeValue.split(/(ECHOx)/g);
+    const fragment = document.createDocumentFragment();
+    parts.forEach(part => {
+      if (part === 'ECHOx') {
+        fragment.append('ECHO');
+        const mark = document.createElement('span');
+        mark.className = 'echox-mark-x';
+        mark.textContent = 'x';
+        fragment.append(mark);
+      } else if (part) {
+        fragment.append(part);
+      }
+    });
+    node.replaceWith(fragment);
+  });
+})();
